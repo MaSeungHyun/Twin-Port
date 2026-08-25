2ca0114d3cbd70b809afb352412a59e05e51d453
마승현 <seunghyun.ma@grapicar.com>
2026-08-25 11:18:28 +0900

    Feature #7813 [Port] 항구 모델 고도화

--- src/views/viewport/_components/Models.tsx
+++ src/views/viewport/_components/Models.tsx
@@ -1,18 +1,18 @@
-import { useObjectStore } from "@/stores/object";
-import { useOccupancyStore } from "@/stores/occupancy";
-import { useViewportStore } from "@/stores/viewport";
-import BlockFootprints from "./Port/Block";
-import BlockOccupancyView from "./Port/BlockOccupancyView";
-import ContainerYard from "./Port/ContainerYard";
+// import { useObjectStore } from "@/stores/object";
+// import { useOccupancyStore } from "@/stores/occupancy";
+// import { useViewportStore } from "@/stores/viewport";
+// import BlockFootprints from "./Port/Block";
+// import BlockOccupancyView from "./Port/BlockOccupancyView";
+// import ContainerYard from "./Port/ContainerYard";
 import Ground from "./Port/Ground";
-import OverHeadCrane from "./Port/OverHeadCrane";
+// import OverHeadCrane from "./Port/OverHeadCrane";
 import ShipFleet from "./Port/ShipFleet";
 
 /** occupancy/container 가시성만 구독 — Ship/Crane 등과 리렌더 분리 */
 function YardLayer() {
-  const containerVisible = useObjectStore((s) => s.containerVisible);
-  const occupancyLook = useOccupancyStore((s) => s.occupancyLook);
-  const tracking = useViewportStore((s) => Boolean(s.selectedContainerId));
+  // const containerVisible = useObjectStore((s) => s.containerVisible);
+  // const occupancyLook = useOccupancyStore((s) => s.occupancyLook);
+  // const tracking = useViewportStore((s) => Boolean(s.selectedContainerId));
 
   return (
     <>
